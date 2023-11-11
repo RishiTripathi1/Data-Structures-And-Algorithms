220f61103eaa2bd6b3b0769c0b187a43de0028eb
RishiTripathi1 <114870015+RishiTripathi1@users.noreply.github.com>
2023-11-11 14:20:40 +0530

    Time: 124 ms (78.74%), Space: 75.5 MB (96.56%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,68 +1,21 @@
 class Solution {
-private:
-
-    vector<int> nextsmaller(vector<int> &arr, int n){
-        stack<int>s;
-        vector<int> ans(n,-1);
-
-        for(int i=n-1;i>=0;i--){
-            int curr = arr[i];
-
-            while(!s.empty()){
-                if(arr[s.top()]<curr){
-                    ans[i]=s.top();
-                    break;
-                }
-                else{
-                    s.pop();
-                }
-            }
-            s.push(i);
-        }
-        return ans;
-    }
-
-    vector<int> prevsmaller(vector<int> &arr, int n){
-        stack<int>s;
-        vector<int> ans(n,-1);
-
-        for(int i=0 ;i<n ;i++){
-            int curr = arr[i];
-
-            while(!s.empty()){
-                if(arr[s.top()]<curr){
-                    ans[i]=s.top();
-                    break;
-                }
-                else{
-                    s.pop();
-                }
-            }
-            s.push(i);
-        }
-        return ans;
-    }
-
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int>next(n); 
-        next = nextsmaller(heights,n);
-        vector<int>prev(n); 
-        prev = prevsmaller(heights,n);
-        int area =INT_MIN;
-        for(int i=0 ;i<n ;i++){
-
-            int length=heights[i];
-
-            if(next[i] == -1){
-                next[i]=n;
+        int n = heights.size();
+        stack<int>s;
+        int ans=0;
+        
+        for(int i=0 ;i<=n ;i++){
+            while(!s.empty() && (i==n || heights[s.top()] >= heights[i])){
+                int height = heights[s.top()];
+                s.pop();
+                int width;
+                if(s.empty()) width=i;
+                else width=i-s.top()-1;
+                ans= max(ans,width*height);
             }
-
-            int breadth = next[i]-prev[i]-1;
-            int newArea= length*breadth;
-            area = max(area,newArea);
+            s.push(i);
         }
-        return area;
+        return ans;
     }
 };
